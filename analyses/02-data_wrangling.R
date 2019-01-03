6b125a490d4685db82a4c46d9726c19020c8aef0
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-01-03 10:01:46 -0500

    overhaul of vivid_epi structure

---
+++ analyses/02-data_wrangling.R
@@ -0,0 +1,300 @@
+#----------------------------------------------------------------------------------------------------
+# Purpose of this script is to wrangle and clean the various covariates in
+# the CD2013, weather, and other datasets
+# 
+# Notes: All variables that I will use will either contain a "_fct" or "_cont" to show
+#        that I have manipulated/investigated that variable.
+#        Men/Women recode combinations (i.e. ha in one and hb in other for same covariate)
+#        will be combined to be hab##
+#----------------------------------------------------------------------------------------------------
+# libraries and imports
+library(tidyverse)
+source("~/Documents/GitHub/VivID_Epi/analyses/00-functions.R")
+load("~/Documents/GitHub/VivID_Epi/data/vividepi_raw.rda")
+dt <- merge_pr_plsmdm_gemtdt(pr = arpr, plsmdm = panplasmpcrres, ge = ge)
+
+#..........................
+# Exposure of Interests
+#..........................
+# SURVEY CHARACTERISTICS/WEIGHTS
+# 1. hv005, cluster level weights
+# 2. hv006, Month of interview
+# 3. hv007, Year of interview
+# 4. hv016, day of interview
+#
+#
+#
+# COINFECTIONS/BIOMARKER VARIABLES
+# 1. pfldh coinfection
+# 2. po18s coinfection
+# 3. HIV Status (HIV03)
+# 4. Currently pregnant? (HA54)
+# 5. Hemoglobin Level adjust for altitude and smoking (HA56 & HB56)
+#
+# SOCIOECOLOGICAL VARIABLES
+# 1. Type of Residence i.e. Rural or Urban (HV025)
+# 2. Drinking Water (categorical: HV201)
+# 4. Main floor material (categorical: HV213)
+# 5. Main wall material (categorical: HV214)
+# 6. Main roof material (categorical: HV215)
+# 7. Shares toilet with other household (HV225)
+# 8. Wealth Index (HV270)
+# 9. Biological Sex (HV104)
+# 10. Age (HV105)
+# 11. Highest year of education completed (continous: HV107) 
+# 12. Number of Children Under Age of 5 (continuous: )
+# 
+# MALARIA-INTERVENTIONS
+# 1. Person slept under an LLIN net (HML20)/Net by Insecticide 
+# 
+# PHYSICAL/LANDSCAPE/CLIMATE VARIABLES
+# 1. Cluster altitude (HV040)
+
+
+#..........................................................................................
+#                                         SURVEY CHARACTERISTICS
+#..........................................................................................
+#.............
+# weights
+#.............
+dt <- dt %>% 
+  dplyr::mutate(hv005_cont = hv005/1e6,
+                hv005 = hv005/1e6,
+                hiv05_cont = hiv05/1e6,
+                hiv05 = hiv05/1e6
+  )
+#.............
+# dates
+#.............
+dt <- dt %>% 
+  dplyr::mutate(hvdate_fct = lubridate::dmy(paste0(hv016, "/", hv006, "/", hv007)))
+
+
+
+
+
+#..........................................................................................
+#                                  COINFECTIONS/BIOMARKER VARIABLES
+#..........................................................................................
+#.............
+# pfldh/po18s
+#.............
+dt <- dt %>% 
+  dplyr::mutate(
+    pfldh_fct = factor(pfldh, levels=c("0", "1"), labels=c("fal+", "fal-")),
+    po18s_fct = factor(po18s, levels=c("0", "1"), labels=c("ov+", "ov-"))
+  )
+
+#.............
+# HIV
+#.............
+# levels(factor(haven::as_factor(dt$hiv03)))
+# hivlabels <- names( attributes(dt$hiv03)$labels )
+# not the hiv03 covariate only has hiv negate or hiv positive so good to go there
+
+dt <- dt %>% 
+  dplyr::mutate(
+    hiv03_fct = haven::as_factor(dt$hiv03)
+  )
+
+#.............
+# pregnant
+#.............
+levels(factor(haven::as_factor(dt$ha54)))
+# preglabels <- names( attributes(dt$ha54)$labels )
+# not the ha54 covariate only has no/don't know or yes, so no extra missing 
+
+dt <- dt %>% 
+  dplyr::mutate(
+    ha54_fct = haven::as_factor(dt$ha54)
+  )
+
+
+#.............
+# hemoglobin
+#.............
+levels(factor(haven::as_factor(dt$ha56)))
+levels(factor(haven::as_factor(dt$hb56)))
+# preglabels <- names( attributes(dt$ha54)$labels )
+# not the ha54 covariate only has no/don't know or yes, so no extra missing 
+dt <- dt %>% 
+  dplyr::mutate(hab56_cont = ifelse(!is.na(ha56), ha56, ifelse(!is.na(hb56), ha56, "error")),
+                hab56_cont = ifelse(hab56_cont %in% c("997", "999"), NA, hab56_cont),
+                hab56_cont = as.numeric(hab56_cont)/10)
+
+
+
+
+#..........................................................................................
+#                                  SOCIOECOLOGICAL VARIABLES
+#...........................................................................................
+#.............
+# Urban
+#.............
+levels(factor(haven::as_factor(dt$ha54)))
+dt$hv025 <- haven::as_factor(dt$hv025)
+
+
+#.............
+# drinking water and non-drink water
+#.............
+levels(factor(haven::as_factor(dt$hv201)))
+
+dt <- dt %>% 
+  dplyr::mutate(hv201_fct = haven::as_factor(hv201), # floor
+                hv201_fct = ifelse(hv201_fct == "missing", NA, hv201_fct)
+  )
+
+
+#.............
+# main floor, wall, roof
+#.............
+dt <- dt %>% 
+  dplyr::mutate(hv213_fct = haven::as_factor(hv213), # floor
+                hv213_fct = ifelse(hv213_fct == "missing", NA, hv213_fct), 
+                
+                hv214_fct = haven::as_factor(hv214), # wall
+                hv214_fct = ifelse(hv214_fct == "missing", NA, hv214_fct), 
+                
+                hv215_fct = haven::as_factor(hv215), # roof
+                hv215_fct = ifelse(hv215_fct == "missing", NA, hv215_fct)
+                # no missing in roof?
+  )
+
+
+
+#.............
+# shares toilet/toilet type
+#.............
+levels(factor(haven::as_factor(dt$hv205)))
+levels(factor(haven::as_factor(dt$hv225)))
+
+dt <- dt %>% 
+  dplyr::mutate(hv205_fct = haven::as_factor(hv205), # toilet facility
+                hv205_fct = ifelse(hv205_fct == "missing", NA, hv205_fct), 
+                
+                hv225_fct = haven::as_factor(hv225), # share toilet yes no
+                hv225_fct = ifelse(hv225_fct == "missing", NA, hv225_fct)
+  )
+
+
+
+#.............
+# wealth index 
+#.............
+levels(factor(haven::as_factor(dt$hv270))) 
+sum(is.na(dt$hv270)) # no missing wealth
+dt <- dt %>% 
+  dplyr::mutate(hv270_fct = haven::as_factor(hv270))
+
+
+#.............
+# sex
+#.............
+levels(factor(haven::as_factor(dt$hv104))) # no missing m/f
+sum(is.na(dt$hv104))
+dt <- dt %>% 
+  dplyr::mutate(hv104_fct = haven::as_factor(hv104))
+
+
+
+#.............
+# age
+#.............
+hist(dt$hv105)
+dt <- dt %>% 
+  dplyr::mutate(hv105_cont = ifelse(hv105 %in% c("97", "98", "99"), NA, hv105))
+
+#.............
+# education
+#.............
+levels(factor(haven::as_factor(dt$hv106)))
+dt <- dt %>% 
+  dplyr::mutate(hv106_fct = haven::as_factor(hv106),
+                hv106_fct = ifelse(hv106_fct %in% c("don't know", "missing"), NA, hv106_fct)
+  )
+
+
+
+
+#------------------------------------------
+# children under 5 number
+#------------------------------------------
+summary(dt$hv014) # looks clean
+dt <- dt %>% 
+  dplyr::mutate(hv014_cont = hv014)
+
+
+#..........................................................................................
+#                                 MALARIA-INTERVENTIONS
+#..........................................................................................
+#.............
+# LLIN
+#.............
+xtabs(~haven::as_factor(dt$hml10) + haven::as_factor(dt$hml20), addNA = T)
+# there are 53 people that slept under ITN" but not LLIN
+
+xtabs(~haven::as_factor(dt$hml19) + haven::as_factor(dt$hml20), addNA = T)
+# there are 122 people that slept under "ever treated net" but not LLIN
+
+xtabs(~haven::as_factor(dt$hml10) + haven::as_factor(dt$hml19), addNA = T)
+# there are 69 people that slept under "ever treated net" but not a ITN
+
+# BASED on this pattern, am just going to consider LLIN
+
+dt <- dt %>% 
+  dplyr::mutate(hml20_fct = haven::as_factor(hml20))
+
+#.............
+# LLIN-type of Inseciticide
+# Note, must have LLIN to have insecticide (120 missing LLIN insecticide types, 8500 no LLIN)
+#.............
+# read insecticide liftover table
+insctcd <- readr::read_csv("internal_datamap_files/pr_insecticide_liftover.csv")
+
+dt <- dt %>% 
+  dplyr::mutate(hml7 = haven::as_factor(hml7)) %>% 
+  left_join(x=., y=insctcd, by="hml7") %>% 
+  dplyr::mutate(insctcd_fct = factor(ifelse(hml20_fct == "no", "none", insctcd)))
+
+# sanity checks
+xtabs(~dt$insctcd_fct + dt$hml20_fct, addNA=T)
+xtabs(~dt$insctcd_fct + dt$hml7, addNA=T)
+xtabs(~dt$hml20_fct + dt$hml7, addNA=T)
+
+#..........................................................................................
+#                                PHYSICAL/LANDSCAPE/CLIMATE VARIABLES
+#..........................................................................................
+
+#.............
+# Cluster-Level Altitude
+#.............
+dt <- dt %>% 
+  dplyr::mutate(hv040_cont = ifelse(hv040 == 9999, NA, hv040))
+
+#.............
+# CLIMATE
+#.............
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#..........................................................................................
+#                               Final Write Out
+#..........................................................................................
+save(dt, file = "data/vividepi_recode.rds")
+
+
--- analyses/04-basic_mapping.R
+++ analyses/04-basic_mapping.R
@@ -8,9 +8,8 @@ library(srvyr) #wrap the survey package in dplyr syntax
 #......................
 # Import Data
 #......................
+load("data/vividepi_recode.rds")
 load("data/vividepi_raw.rda")
-dt <- merge_pr_plsmdm_gemtdt(pr = arpr, plsmdm = panplasmpcrres, ge = ge)
-
 
 # R OSM
 # http://osmar.r-forge.r-project.org/
@@ -21,47 +20,13 @@ dt <- merge_pr_plsmdm_gemtdt(pr = arpr, plsmdm = panplasmpcrres, ge = ge)
 # Datawrangle
 #......................
 dtsub <- dt %>% 
-  dplyr::select(c("hivrecode_barcode", "hv001", "hv002", "hv005", "hiv05", "pfldh", "po18s", "pv18s", "adm1dhs", "adm1name", "latnum", "longnum")) %>% 
-  dplyr::mutate(hv005 = hv005/1e5,
-                hiv05 = hiv05/1e5) # per the DHS website https://dhsprogram.com/data/Using-DataSets-for-Analysis.cfm#CP_JUMP_14042 --- like other variables in DHS datasets, decimal points are not included in the weight variable. Analysts need to divide the sampling weight they are using by 1,000,000
-#   tidyr::gather(., key="plsmdmspec", value = "outcome", 6:8) %>%
-#   dplyr::group_by(plsmdmspec) %>% 
-#   tidyr::nest() 
-# 
-# 
-# dtsub_summ <- dplyr::bind_cols(dplyr::bind_rows(dtsub, dtsub), maplvl = c(rep("adm1name", nrow(dtsub)), rep("hv001", nrow(dtsub)))) %>% 
-#   dplyr::mutate(sfobj = ifelse(maplvl == "adm1name", "DRCprov", 
-#                                  ifelse(maplvl == "hv001", "ge", 
-#                                         NA))
-#   )
+  dplyr::select(c("hivrecode_barcode", "hv001", "hv002", "hiv05_cont", "pfldh", "po18s", "pv18s", "adm1dhs", "adm1name", "latnum", "longnum"))
 
 #----------------------------------------------------------------------------------------------------
 # Explore here the different prevalences/regions of Plasmodium species basic maps
 #----------------------------------------------------------------------------------------------------
 # issue here is that we are not using the weights. easy to incorporate...but is it worthwhile
 # at the very least, need to include the HIV weights
-
-prev_summarizer <- function(data, maplvl, plsmdmspec, sfobj){
-  
-  
-  # catch error
-  # if( !( any(colnames(x) %in% c("hv001")) & any(colnames(x) %in% c("hv005")) ) ){
-  #   stop("Must include cluster and cluster weight. need to check if you are using pr or hiv sample weights")
-  # }
-  # rlang
-  maplvl <- enquo(maplvl)
-  plsmdmspec <- enquo(plsmdmspec)
-  
-  # clusters are weighted (each individual has same weight in cluster)
-  ret <- data %>% 
-    srvyr::as_survey_design(ids = hv001, weights = hv005) %>% 
-    dplyr::group_by(!!maplvl) %>% 
-    dplyr::summarise(plsmdn = srvyr::survey_total(hv001), 
-                     plsmd = srvyr::survey_mean(!!plsmdmspec, na.rm = T, vartype = c("se", "ci"), level = 0.95)) %>% 
-    dplyr::left_join(., sfobj) # attach spatial data, let R figure out the common var
-  # return
-  return(ret)
-}
 
 
 # set mapping parameters
@@ -77,27 +42,30 @@ prev_summarizer <- function(data, maplvl, plsmdmspec, sfobj){
 # NOTE PURRR is having an issue because of the "~"
 
 
-pfldhprov <- prev_summarizer(data = dtsub, maplvl = adm1name, plsmdmspec = pfldh, sfobj = DRCprov) %>% 
+pfldhprov <- prev_point_est_summarizer(data = dtsub, maplvl = adm1name, plsmdmspec = pfldh, sfobj = DRCprov) %>% 
   dplyr::mutate(plsmdmspec = "pfldh", maplvl = "adm1name")
-pv18sprov <- prev_summarizer(data = dtsub, maplvl = adm1name, plsmdmspec = pv18s, sfobj = DRCprov)  %>% 
+pv18sprov <- prev_point_est_summarizer(data = dtsub, maplvl = adm1name, plsmdmspec = pv18s, sfobj = DRCprov)  %>% 
   dplyr::mutate(plsmdmspec = "pv18s", maplvl = "adm1name")
-po18sprov <- prev_summarizer(data = dtsub, maplvl = adm1name, plsmdmspec = po18s, sfobj = DRCprov) %>% 
+po18sprov <- prev_point_est_summarizer(data = dtsub, maplvl = adm1name, plsmdmspec = po18s, sfobj = DRCprov) %>% 
   dplyr::mutate(plsmdmspec = "po18s", maplvl = "adm1name")
 
-pfldhclust <- prev_summarizer(data = dtsub, maplvl = hv001, plsmdmspec = pfldh, sfobj = ge) %>% 
+pfldhclust <- prev_point_est_summarizer(data = dtsub, maplvl = hv001, plsmdmspec = pfldh, sfobj = ge) %>% 
   dplyr::mutate(plsmdmspec = "pfldh", maplvl = "hv001")
-pv18sclust <- prev_summarizer(data = dtsub, maplvl = hv001, plsmdmspec = pv18s, sfobj = ge) %>% 
+pv18sclust <- prev_point_est_summarizer(data = dtsub, maplvl = hv001, plsmdmspec = pv18s, sfobj = ge) %>% 
   dplyr::mutate(plsmdmspec = "pv18s", maplvl = "hv001")
-po18sclust <- prev_summarizer(data = dtsub, maplvl = hv001, plsmdmspec = po18s, sfobj = ge) %>% 
+po18sclust <- prev_point_est_summarizer(data = dtsub, maplvl = hv001, plsmdmspec = po18s, sfobj = ge) %>% 
   dplyr::mutate(plsmdmspec = "po18s", maplvl = "hv001")
 
 
-# this awful hack becuase of this issue https://github.com/tidyverse/dplyr/issues/3483
-# we are going down the rabbit hole just to try and make this stupid survey and purr package work. fine for now but return
+
+# bind those to a tibble
 mp <- dplyr::bind_rows(pfldhprov, pv18sprov, po18sprov, pfldhclust, pv18sclust, po18sclust) %>% 
   dplyr::group_by(plsmdmspec, maplvl) %>% 
   tidyr::nest()
 
+
+# this awful hack becuase of this issue https://github.com/tidyverse/dplyr/issues/3483
+# we are going down the rabbit hole just to try and make this stupid survey and purr package work. fine for now but return
 mp$data <- sapply(list(pfldhprov, pv18sprov, po18sprov, pfldhclust, pv18sclust, po18sclust), function(x) return(x))
 
 
@@ -110,27 +78,18 @@ mapplotter <- function(data, maplvl, plsmdmspec){
   ret <- ggplot() + 
     geom_sf(data = DRCprov) +
     ggtitle(paste(plsmdmspec)) +
-    theme(plot.title = element_text(famil = "Arial", face = "bold", hjust = 0.5, size = 13), 
-          legend.position = "bottom",
-          legend.title = element_text(famil = "Arial", face = "bold", vjust = 0.85, size = 12),
-          legend.text = element_text(famil = "Arial", hjust = 0.5, vjust = 0.5, angle = 90, size = 10),
-          axis.text = element_blank(),
-          axis.line = element_blank(),
-          panel.background = element_rect(fill = "transparent"),
-          plot.background = element_rect(fill = "transparent"),
-          panel.grid = element_blank(),
-          panel.border = element_blank())
+    vivid_map_theme
   
   if(maplvl == "adm1name"){
     
     ret <- ret + geom_sf(data = data, aes(fill = plsmd)) +
-      scale_fill_gradient2("Prevalence", low = "#0000FF", mid = "#FFEC00", high = "#FF0000", midpoint = quantile(data$plsmd[data$plsmd != 0], 0.75)) + 
+      scale_fill_gradient2("Prevalence", low = "#0000FF", mid = "#FFEC00", high = "#FF0000") + 
       coord_sf(datum=NA)  # to get rid of gridlines
     
   } else if(maplvl == "hv001"){
     
     ret <- ret + geom_sf(data = data, aes(fill = plsmd, colour = plsmd, size = plsmdn), alpha = 0.8) +
-      scale_color_gradient2("Prevalence", low = "#0000FF", mid = "#FFEC00", high = "#FF0000", midpoint = quantile(data$plsmd[data$plsmd != 0], 0.75)) + 
+      scale_color_gradient2("Prevalence", low = "#0000FF", mid = "#FFEC00", high = "#FF0000") + 
       scale_size(guide = 'none') +  scale_fill_continuous(guide = 'none') +
       coord_sf(datum=NA)  # to get rid of gridlines
     
@@ -145,7 +104,7 @@ mapplotter <- function(data, maplvl, plsmdmspec){
 
 prevmaps <- pmap(mp, mapplotter)
 
-jpeg(file = "figures/04-prevmaps.jpg", width = 11, height = 8, units = "in", res=300)
+# jpeg(file = "figures/04-prevmaps.jpg", width = 11, height = 8, units = "in", res=300)
 gridExtra::grid.arrange(prevmaps[[1]], 
                         prevmaps[[2]],
                         prevmaps[[3]],
@@ -153,11 +112,12 @@ gridExtra::grid.arrange(prevmaps[[1]],
                         prevmaps[[5]],
                         prevmaps[[6]],
                         nrow=2, top=grid::textGrob("Prevalence by Species in CD2013 DHS", gp=grid::gpar(fontsize=15, fontfamily = "Arial", fontface = "bold"))) 
-graphics.off()
+# graphics.off()
 
 #----------------------------------------------------------------------------------------------------
 # Explore here the different clusters of Plasmodium species z scores
-
+#----------------------------------------------------------------------------------------------------
+#
 clusters <- mp %>% 
   dplyr::filter(maplvl == "hv001")
 
@@ -188,16 +148,7 @@ zscoremapplotter <- function(data, plsmdmspec){
     scale_size(guide = 'none') +
     ggtitle(paste(plsmdmspec)) +
     coord_sf(datum=NA) + # to get rid of gridlines
-    theme(plot.title = element_text(famil = "Arial", face = "bold", hjust = 0.5, size = 18), 
-          legend.position = "bottom",
-          legend.title = element_text(famil = "Arial", face = "bold", vjust = 0.85, size = 12),
-          legend.text = element_text(famil = "Arial", hjust = 0.5, vjust = 0.5, angle = 90, size = 10),
-          axis.text = element_blank(),
-          axis.line = element_blank(),
-          panel.background = element_rect(fill = "transparent"),
-          plot.background = element_rect(fill = "transparent"),
-          panel.grid = element_blank(),
-          panel.border = element_blank())
+    vivid_map_theme
   
   return(ret)
   
@@ -211,24 +162,12 @@ prevhist <- pmap(list(data = clusters$ret, plsmdmspec = clusters$plsmdmspec), fu
   ggplot(.) +
     geom_histogram(mapping = aes(x=plsmd)) + 
     ggtitle(paste(plsmdmspec)) + ylab("Count") +
-    theme(plot.title = element_text(famil = "Arial", face = "bold", hjust = 0.5, size = 18), 
-          legend.position = "bottom",
-          legend.title = element_text(famil = "Arial", face = "bold", vjust = 0.85, size = 12),
-          legend.text = element_text(famil = "Arial", hjust = 0.5, vjust = 0.5, angle = 90, size = 10),
-          axis.title.x = element_blank(),
-          axis.text.x = element_text(famil = "Arial", hjust = 0.5, vjust = 0.5, angle = 60, size = 10),
-          axis.text.y = element_text(famil = "Arial", hjust = 0.5, vjust = 0.5, size = 10),
-          axis.ticks = element_blank(),
-          axis.line = element_line(colour = "black"),
-          panel.background = element_rect(fill = "transparent"),
-          plot.background = element_rect(fill = "transparent"),
-          panel.grid = element_blank(),
-          panel.border = element_blank())
+    vivid_map_theme
 })
 
 
 
-jpeg(file = "figures/04-zscoremaps.jpg", width = 11, height = 8, units="in", res=300)
+# jpeg(file = "figures/04-zscoremaps.jpg", width = 11, height = 8, units="in", res=300)
 gridExtra::grid.arrange(prevhist[[1]],
                         prevhist[[2]],
                         prevhist[[3]],
@@ -237,5 +176,5 @@ gridExtra::grid.arrange(prevhist[[1]],
                         zscoreprevmaps[[3]],
                         nrow=2, 
                         top=grid::textGrob("Histograms and Standardized Prevalence by Species in CD2013 DHS", gp=grid::gpar(fontsize=15, fontfamily = "Arial", fontface = "bold"))) 
-graphics.off()
+# graphics.off()
 
