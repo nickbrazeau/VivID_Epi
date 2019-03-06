a53d7da8469e2daf9669c0d6a3c33c2e0e39575d
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-03-06 13:38:36 -0500

    update covariates and covariate search

---
+++ R/00-functions_seasonality.R
@@ -0,0 +1,65 @@
+# adapted from OJWatson/magenta
+# extracted from Cairns et al. 2012 PMC3621394 
+
+calc_seasonality <- function(drc_admin_units_seasonal) {
+  
+  
+  ## extract fourier covariates
+  ssa0 <- drc_admin_units_seasonal$seasonal_a0
+  ssa1 <- drc_admin_units_seasonal$seasonal_a1
+  ssa2 <- drc_admin_units_seasonal$seasonal_a2
+  ssa3 <- drc_admin_units_seasonal$seasonal_a3
+  ssb1 <- drc_admin_units_seasonal$seasonal_b1
+  ssb2 <- drc_admin_units_seasonal$seasonal_b2
+  ssb3 <- drc_admin_units_seasonal$seasonal_b3
+  # theta_c <- drc_admin_units_seasonal$seasonal_theta_c
+  theta_c <- drc_admin_units_seasonal$ft # TEMP -- this likely isn't correct
+    
+  # define vector of times spanning one year
+    tvec = 1:365
+    
+    # calculate Fourier series
+    y <- sapply(t, function(x) {
+      max((ssa0 + 
+             ssa1 * cos(2 * pi * x / 365) + 
+             ssa2 * cos(2 * 2 * pi * x / 365) + 
+             ssa3 * cos(3 * 2 * pi * x / 365) + 
+             ssb1 * sin(2 * pi * x / 365) + 
+             ssb2 * sin(2 * 2 * pi * x / 365) + 
+             ssb3 * sin(3 * 2 * pi * x / 365)) / theta_c, 0.001) # note the max 0.0001 ensures that scaling factor never goes below zero (this can happen in practice because we are only using the first few terms in an infinite series)
+    })
+  
+    # now have 365 days of seasonality
+  return(y)
+}
+
+# now add by month and admin level
+# still adapted from OJWatson/magenta
+
+####### 
+Issue with how regions are being linked
+####### 
+
+
+add_seasonal <- function(fourier_transform_365days, months, admin1){
+    if(is.na(months)){
+      return(NA)
+    }
+    months <- as.numeric(months)
+    region <- names(attr(admin1,"labels"))[as.numeric(admin1)]
+    ring <- c(12,1:11)
+    mts <- ring[months]
+    days <- lubridate::days_in_month(1:12) %>% cumsum
+    days <- sapply(1:12,function(x) c(0,days[-12])[x]:days[x])
+    return(mean(fourier_transform_365days[unlist(days[mts]),region]))
+  }
+  
+  
+  
+dt$seasonal_adm_month <- apply(dt,
+                               1,
+                               seasonal_add,   
+                                  fourier_transform_365days = calc_seasonality(drc_admin_units_seasonal),
+                                  months = dt$hv006,
+                                  admin1 = dt$hv024)
+                                 
--- analyses/01-data_import_space.R
+++ analyses/01-data_import_space.R
@@ -6,6 +6,10 @@
 library(tidyverse)
 devtools::install_github("ropensci/osmdata")
 library(osmdata)
+devtools::install_github("malaria-atlas-project/malariaAtlas")
+library(malariaAtlas)
+devtools::install_github("OJWatson/magenta")
+library(magenta)
 library(sf)
 source("~/Documents/GitHub/VivID_Epi/R/00-functions_basic.R")
 #---------------------------------------------------------------------------------
@@ -59,11 +63,14 @@ brdrcnt <- lapply(c("UGA", "SSD", "CAF", "COG", "AGO", "ZMB", "TZA", "RWA", "BDI
 #   sf::st_union(., brdrcnt[[11]], by_feature = T) %>% 
 #   sf::st_union(., brdrcnt[[12]])
 # unfortunately this knocks out borders (logically)... will have to keep as seperate layers
+
 #..............................
 # Pull down ocean
 #..............................
 # https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_ocean.zip
 oceans <- sf::st_read("data/ne_10m_ocean/ne_10m_ocean.shp")
+
+
 #---------------------------------------------------------------------------------
 # pull down OSM maps
 #---------------------------------------------------------------------------------
@@ -180,12 +187,10 @@ ape <- primate[grepl("pan paniscus|pan troglodytes|gorilla", tolower(primate$BIN
 # https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0056487
 # https://www.nature.com/articles/ncomms1879#supplementary-information
 # https://betterexplained.com/articles/an-interactive-guide-to-the-fourier-transform/
-load("data/imperial_share/admin_units_seasonal.rda")
-admin_units_seasonal <- admin_units_seasonal %>% 
-  filter(country == "Democratic Republic of the Congo")
-# this is from 2010 and just the 13 DRC prov... 
+admin_units_seasonal <- readRDS("data/imperial_share/admin1_seasonality_for_nick_from_OJ.rds")
+drc_admin_units_seasonal <- admin_units_seasonal %>% 
+  filter(COUNTRY_ID == "COD")
 
-# if this doesn't pan out, check the GSODR package from Ropensci -- https://cran.r-project.org/web/packages/GSODR/index.html
 
 
 #---------------------------------------------------------------------------------
@@ -197,6 +202,22 @@ gc <- readr::read_csv("datasets/CDGC62FL/CDGC62FL.csv") %>%
   dplyr::rename(hv001 = dhsclust) # for easier merge with PR
 
 
+#---------------------------------------------------------------------------------
+# Rainfall Data from OJ for admin1 in DHS
+#---------------------------------------------------------------------------------
+# Daily DRC's real rainfall data from CHIRPs for 2013-2017
+
+rain <- readRDS(file = "~/Documents/GitHub/VivID_Epi/data/imperial_share/drc_rainfall_2013-2017.rds")
+
+ 
+#---------------------------------------------------------------------------------
+# MAP project for R 
+#---------------------------------------------------------------------------------
+# from their example code chunk
+# download raster of travel time to cities (Weiss et al 2018) for study area & visualise this
+TravelToCities <- malariaAtlas::getRaster(surface = "A global map of travel time to cities to assess inequalities in accessibility in 2015",
+                                  extent = bb)
+TravelToCities <- log(TravelToCities+0.1) # MAP project decided 0.1 for offest. going to keep it
 #---------------------------------------------------------------------------------
 # write out small objects 
 #---------------------------------------------------------------------------------
