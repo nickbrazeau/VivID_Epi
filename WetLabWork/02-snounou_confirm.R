964c09d9ba166865648998416fa19fdb2f2ab70b
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-06-03 20:21:57 -0400

    confirmatory snounou

--- WetLabWork/02-snounou_confirm.R
+++ WetLabWork/02-snounou_confirm.R
@@ -1,5 +1,5 @@
 #---------------------------------------------------------------------------
-# Purpose of this script is to randomly select 94 samples
+# Purpose of this script is to randomly select 95 samples
 # (e.g. one plate worth plus pos control and NTC)
 # from the snounou plates to confirm Pv 18s with sanger sequencing
 #---------------------------------------------------------------------------
@@ -29,13 +29,6 @@ sn <- sn %>%
 
 
 
-adult_snounou <- readxl::read_excel(path = "/Volumes/share/1. Data/1. Raw Data/Adult_Pv18s/Snounou_Verification_v4.xlsx", sheet = 2)
-adult_snounou <- adult_snounou %>% 
-  dplyr::mutate(original_plate = stringr::str_split_fixed(SampleName, "_", n=3)[,1],
-                original_plate_loc = stringr::str_split_fixed(SampleName, "_", n=3)[,2],
-                barcode = stringr::str_split_fixed(SampleName, "_", n=3)[,3]) %>% 
-  dplyr::mutate(barcode = tolower(barcode)) # all lower cases, to remove Case Sensitivity
-
 
 #---------------
 # 96 well plate specs
@@ -43,27 +36,35 @@ adult_snounou <- adult_snounou %>%
 plt_rowlet <- sort(rep(LETTERS[1:8], 12))
 plt_colnum <- rep(seq(1:12), 8)
 
+
+
+
 #---------------
-# Pulli in recode so this is from the actual 16363 smpls 
+# Subset to plates that we still have
+#---------------
+sn <- sn %>% 
+  dplyr::filter(plate %in% c("VivID_M021-M041", "VivID_M042-M062_plate_1", "VivID_M042-M062_plate_2", "VivID_M063-M084", 
+                             "VivID_M124-M143", "VivID_M144-M163", "VivID_M164-M183"))
+
+
+#---------------
+# Pull in recode so this is from the actual smpls being used (and know they were snounou positive already)
 #---------------
 dt <- readRDS("~/Documents/GitHub/VivID_Epi/data/derived_data/vividepi_recode.rds")
-pvpos <- dt[dt$pv18s_fctb == "vivpos",]
+pvpos <- dt[dt$pv18s_fctb == "vivpos",] %>% 
+  dplyr::rename(barcode = hivrecode_barcode)
 
 
-#---------------------------------------------------------------------------------------------------------
-# Randomly select 94 samples for DUFFY actually being used in this study from the 16,363
-#---------------------------------------------------------------------------------------------------------
-pvsnounoubarcodes <- sample(pvpos$hivrecode_barcode, size = 94, replace = F) %>% 
-  tibble(barcode = .)
 
+# note, this already isn't a truly random sample since we have plates that are 
+# missing. So going to ignore the issue with some samples being lost to barcode
+# typos as well and just select 95 samples that I know are being used by my study
+# and we have sample left to sanger sequene
 
-#---------------
-# Find matches in SN
-#---------------
-sangersnounou <- inner_join(x=sn, y=pvsnounoubarcodes, by = "barcode")
-if(nrow(sangersnounou) != 94){
-  stop("There was a barcode matching error. See which one it has a one-off base with as you did in the original data generation")
-}
+snounouret <- dplyr::inner_join(pvpos[,"barcode"], sn)
+snounouret <- sample(snounouret$barcode, size = 95, replace = F)
+sangersnounou <- sn %>% 
+  dplyr::filter(barcode %in% snounouret)
 
 #---------------
 # write this out for Nick in a way that is easier to work with 
@@ -85,15 +86,30 @@ sangersnounou %>%
   dplyr::select(c("sngplt_rowlet", "sngplt_colnum", "snounouname")) %>% 
   rbind.data.frame(. ,
                    data.frame(
-                     sngplt_rowlet = c("H", "H"),
-                     sngplt_colnum =  c(11,12),
-                     snounouname = c("PC", "NTC"))
+                     sngplt_rowlet = c("H"),
+                     sngplt_colnum =  c(12),
+                     snounouname = c("PC"))
   ) %>% 
   tidyr::spread(., key = "sngplt_colnum", value = "snounouname") %>% 
   write.csv(., file = "WetLabWork/platemapsout/confirmatory_Pv18s_Snounou_sanger_smpls_WIDE.csv",
             quote = F, row.names = F)
 
 
+
+
+
+# now write out barcode for the Sanger Submission
+sangersnounou %>% 
+  dplyr::select(c("sngplt_rowlet", "sngplt_colnum", "barcode")) %>% 
+  rbind.data.frame(. ,
+                   data.frame(
+                     sngplt_rowlet = c("H"),
+                     sngplt_colnum =  c(12),
+                     barcode = c("PC"))
+  ) %>% 
+  tidyr::spread(., key = "sngplt_colnum", value = "barcode") %>% 
+  write.csv(., file = "WetLabWork/platemapsout/Sanger_Submission_Sheet_confirmatory_Pv18s_Snounou_sanger_smpls_WIDE.csv",
+            quote = F, row.names = F)
 
 
 
