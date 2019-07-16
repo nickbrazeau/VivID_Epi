731083bf2cc2e0341c0de517f4637facce74440c
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-07-16 18:43:54 -0400

    IPTW work for corr

--- analyses/06-IPW_ML/02-ApplyIPTW_RR.R
+++ analyses/06-IPW_ML/02-ApplyIPTW_RR.R
@@ -2,8 +2,24 @@
 # Purpose of this script is to apply IPTW weights
 #----------------------------------------------------------------------------------------------------
 library(tidyverse)
-
+library(mlr)
 
 #......................................
 # Read in param table sent to slurm
 #......................................
+resultsdir <- "~/Documents/MountPoints/mountedMeshnick/Sandbox/VivID_Epi/_rslurm_vivid_preds/"
+
+results.list <- list.files(resultsdir, pattern = "results", full.names = T)
+results <- lapply(results.list, function(x){
+  
+  ret <- readRDS(x)
+  names(ret) <- basename(x)
+  return(ret)
+}) %>% 
+  dplyr::bind_rows(., .id = "retname")
+
+
+params <- readRDS(file = paste0(resultsdir, "params.RDS"))
+
+
+
