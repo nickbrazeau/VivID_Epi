7f5b0eaa02d422763ed2ac670025bf15f8fbbc25
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-07-21 19:33:03 -0400

    update slurm scripts for LL

---
+++ analyses/00-slurm/find_tunes_on_LL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --time=5-00:00:00
+#SBATCH --mem=49512
+#SBATCH --mail-type=all
+#SBATCH --mail-user=nbrazeau@med.unc.edu
+
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Epi"); source("analyses/06-IPW_ML/01-make_tune_IPTWModels.R")'
