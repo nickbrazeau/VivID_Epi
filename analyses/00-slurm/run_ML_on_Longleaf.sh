cbfb0ce34ee5d0aa98507badf52052b2d50b7ce4
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-07-21 17:07:48 -0400

    update slurm commands

--- analyses/00-slurm/run_ML_on_Longleaf.sh
+++ analyses/00-slurm/run_ML_on_Longleaf.sh
@@ -6,4 +6,5 @@
 #SBATCH --mail-type=all
 #SBATCH --mail-user=nbrazeau@med.unc.edu
 
-Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Epi"); source("analyses/06-IPW_ML/01-make_tune_IPTWModels.R")'
+# Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Epi"); source("analyses/06-IPW_ML/01-make_tune_IPTWModels.R")'
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Epi"); source("analyses/06-IPW_ML/02-ApplyTunes_MakeFinalMod.R")'
