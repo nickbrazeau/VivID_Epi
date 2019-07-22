fd9e0e842341bcadc830ab00f366c0baa8cca621
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-07-22 06:01:50 -0400

    fix reps

--- analyses/06-IPW_ML/01-make_tune_IPTWModels.R
+++ analyses/06-IPW_ML/01-make_tune_IPTWModels.R
@@ -146,7 +146,7 @@ txs$performmeasure <- map2(txs$performmeasure, txs$nulldist, function(x, y){
 ###################################################
 ###################################################
 # resampling approach with spatial CV considered
-rdesc <- makeResampleDesc("SpRepCV", fold = 3, reps = 1)
+rdesc <- makeResampleDesc("SpRepCV", fold = 3, reps = 3)
 txs$rdesc <- lapply(1:nrow(txs), function(x) return(rdesc))
 
 
