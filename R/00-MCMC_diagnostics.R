6d29d943ca8628c8af82d8f5e707dd0ebdb5cc65
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-07-22 19:59:49 -0400

    icar

---
+++ R/00-MCMC_diagnostics.R
@@ -0,0 +1,36 @@
+# TODO add more diagnostics
+# https://www2.stat.duke.edu/courses/Fall09/sta290/Lectures/Diagnostics/param-diag.pdf
+# consider adding ACF plots
+# effectiveSize(theta.MCMC)
+
+
+
+# checkConvergence
+# calculates Geweke statistic from a series of burn-in and sampling draws. Report whether burn-in length was sufficient based on this statistic.
+# (not exported)
+
+checkConvergence <- function(burnin, samples) {
+  
+  # get number of burnin and sampling iterations
+  nburnin <- length(burnin)
+  nsamples <- length(samples)
+  
+  # calculate Geweke diagnostic on combined chain
+  chain <- coda::mcmc(c(burnin, samples))
+  geweke_z <- coda::geweke.diag(chain, frac1 = nburnin/(nburnin+nsamples), frac2 = nsamples/(nburnin+nsamples))$z
+  
+  if(is.na(geweke_z)){
+    stop("NaN p-value was calculated from Geweke statistic")
+  }
+  
+  # convert to p-value
+  geweke_p <- 2*pnorm(abs(geweke_z), lower.tail=FALSE)
+  
+  # report convergence
+  if (geweke_p > 0.05) {
+    cat(paste0("convergence reached within defined burn-in period (Geweke p=", round(geweke_p, 3), ")"))
+  } else {
+    cat(paste0("WARNING: convergence not reached within defined burn-in period (Geweke p=", round(geweke_p,3), ")"))
+  }
+  
+}
--- analyses/08-spatial_prediction/01-areal_map_prediction.R
+++ analyses/08-spatial_prediction/01-areal_map_prediction.R
@@ -2,8 +2,9 @@
 # Purpose of this script is to make ICAR models
 # Will aggregate up to the province level
 #----------------------------------------------------------------------------------------------------
-source("~/Documents/GitHub/VivID_Epi/R/00-functions_basic.R") 
-source("~/Documents/GitHub/VivID_Epi/R/00-functions_epi.R") 
+source("R/00-functions_basic.R") 
+source("R/00-functions_epi.R") 
+source("R/00-MCMC_diagnostics.R")
 library(tidyverse)
 library(srvyr) #wrap the survey package in dplyr syntax
 library(CARBayes)
@@ -19,56 +20,63 @@ mp <- readRDS("data/derived_data/basic_cluster_mapping_data.rds")
 #......................
 # Subset to Pv
 #......................
-pvprov <- mp %>% 
+pvprov.weighted <- mp %>% 
   dplyr::filter(plsmdmspec == "pv18s" & maplvl == "adm1name") %>% 
   dplyr::select(data) %>% 
   tidyr::unnest()
 # vectors have destroyed spatial class, need to remake
-pvprov <- sf::st_as_sf(pvprov)
+pvprov.weighted <- sf::st_as_sf(pvprov.weighted)
+# need to keep integers, so will round
+pvprov.weighted <- pvprov.weighted %>% 
+  dplyr::mutate_if(is.numeric, round, 0)
+
+
+
 
 #-------------------------------------------------------------------------
 # Aggregate Covariates
 #-------------------------------------------------------------------------
 
 prov.covar <- dtsrvy %>% 
-  dplyr::mutate(count = 1) %>% 
   dplyr::group_by(adm1name) %>% 
-  dplyr::summarise(n = srvyr::survey_total(count), 
-                   meanprov_precip_lag_cont_scale_clst = srvyr::survey_mean(precip_lag_cont_scale_clst, na.rm = T, vartype = c("se", "ci"), level = 0.95),
+  dplyr::summarise(meanprov_precip_lag_cont_scale_clst = srvyr::survey_mean(precip_lag_cont_scale_clst, na.rm = T, vartype = c("se", "ci"), level = 0.95),
                    meanprov_alt_dem_cont_scale_clst = srvyr::survey_mean(alt_dem_cont_scale_clst, na.rm = T, vartype = c("se", "ci"), level = 0.95)
   )
 
-pvprov <- dplyr::left_join(pvprov, prov.covar, by = "adm1name")
+pvprov.weighted <- dplyr::left_join(pvprov.weighted, prov.covar, by = "adm1name")
+pvprov.weighted.nosf <- pvprov.weighted
+sf::st_geometry(pvprov.weighted.nosf) <- NULL
+
 
 #-------------------------------------------------------------------------
 # Basic Multilevel Model
 #-------------------------------------------------------------------------
-# https://github.com/tmalsburg/MCMCglmm-intro
-library(MCMCglmm)
-
-
-
-prior <- list(
-  R=list(V=1, n=1, fix=1),
-  G=list(G1=list(V        = diag(8),
-                 n        = 8,
-                 alpha.mu = rep(0, 8),
-                 alpha.V  = diag(8)*25^2),
-         G2=list(V        = diag(4),
-                 n        = 4,
-                 alpha.mu = rep(0, 4),
-                 alpha.V  = diag(4)*25^2)))
-
-m3 <- MCMCglmm(pronoun ~ (a + b + c)^3,
-               ~ us(1 + (a + b + c)^3):subject +
-                 us(1 + (a + b    )^2):item,
-               data   = d,
-               family = "categorical",
-               prior  = prior.m3,
-               thin   = 1,
-               burnin = 3000,
-               nitt   = 4000)
-
+# # https://github.com/tmalsburg/MCMCglmm-intro
+# # https://cran.r-project.org/web/packages/MCMCglmm/vignettes/Overview.pdf
+# # https://ms.mcmaster.ca/~bolker/R/misc/foxchapter/bolker_chap.html
+# library(MCMCglmm)
+# 
+# # Random Intercept model, no covariates
+# prior.randint <- list(R = list(V = diag(1), nu = 1e-3),
+#                       G = list(G1 = list(V = diag(1), nu = 1, 
+#                                          alpha.mu = 0, alpha.V = diag(1)*1e2)))
+# post.randint <- MCMCglmm(cbind(plsmdn, n) ~ 1, 
+#                          random = ~adm1name,
+#                          prior = prior.randint, 
+#                          data = pvprov.weighted.nosf, 
+#                          family = "multinomial2",
+#                          nitt = 1e5,
+#                          burnin = 1e3)
+# 
+# # check model fit
+# coda::autocorr(post.randint$Sol)
+# coda::effectiveSize(post.randint$Sol)
+# plot(post.randint$Sol)
+# 
+# # pretty miserable looking, likely because too little data?
+# # Within an MCMCglmm fit the chains are stored in two separate matrices (mcmc objects, actually, but these can be treated a lot like matrices) called  Sol (fixed effects) and VCV (variances and covariances). (Random effects are not saved unless you set pr=TRUE.)
+# allChains <- as.mcmc(cbind(post.randint$Sol,post.randint$VCV))
+# plot(allChains)
 
 #-------------------------------------------------------------------------
 # Conditional Autoregressive Spatial Model 
@@ -77,8 +85,54 @@ m3 <- MCMCglmm(pronoun ~ (a + b + c)^3,
 # Make Adjacency Matrix for Pv 
 #......................
 # https://cran.r-project.org/web/packages/spdep/vignettes/nb.pdf
-W.nb <- spdep::poly2nb(sf::as_Spatial(pvprov), row.names = rownames(sf::as_Spatial(pvprov)@data), style = "B") # binary weights taking values zero or one (only one is recorded)
+W.nb <- spdep::poly2nb(sf::as_Spatial(pvprov.weighted), row.names = pvprov$adm1name)
+W <- spdep::nb2mat(W.nb, style = "B") # binary weights taking values zero or one (only one is recorded)
 
+
+#......................
+# Fit Random Intercept (set rho to 0) 
+#......................
+rand.int <- CARBayes::S.CARleroux(formula = plsmdn~1 , 
+                                  family = "binomial", 
+                                  trials = pvprov.weighted.nosf$n, 
+                                  W = W,
+                                  rho = 0,
+                                  data = pvprov.weighted.nosf,
+                                  burnin = 10, 
+                                  n.sample = 50)
+
+
+#......................
+# Fit Complete Spatial Dependence (set rho to 1) 
+#......................
+spat.int <- CARBayes::S.CARleroux(formula = plsmdn~1 , 
+                                  family = "binomial", 
+                                  trials = pvprov.weighted.nosf$n, 
+                                  W = W,
+                                  rho = 1,
+                                  data = pvprov.weighted.nosf,
+                                  burnin = 10, 
+                                  n.sample = 50)
+
+
+
+#......................
+# Fit CARleroux with rho being estimated
+#......................
+CARleroux <- CARBayes::S.CARleroux(formula = plsmdn~1 , 
+                                  family = "binomial", 
+                                  trials = pvprov.weighted.nosf$n, 
+                                  W = W,
+                                  rho = NULL,
+                                  data = pvprov.weighted.nosf,
+                                  burnin = 10, 
+                                  n.sample = 50)
+
+model.spatial$fitted.values
+length(model.spatial$fitted.values)
+length(Y)
+
+plot(Y ~ model.spatial$fitted.values)
 
 
 
