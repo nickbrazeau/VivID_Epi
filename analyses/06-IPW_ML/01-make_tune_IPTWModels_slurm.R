90845b82fe77d34d1a272330059f48cc2ece3b30
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-08-01 13:26:42 -0400

    slurm models

--- analyses/06-IPW_ML/01-make_tune_IPTWModels_slurm.R
+++ analyses/06-IPW_ML/01-make_tune_IPTWModels_slurm.R
@@ -34,7 +34,7 @@ txs <- txs %>%
 # manipulate data
 #........................
 # subset to treatments, outcome, weights and coords
-varstoinclude <- c("pv18s" , "pfldh", "hv005_wi", txs$target, txs$column_name, "hvyrmnth_dtmnth_lag",
+varstoinclude <- c("pv18s" , "pfldh", "hv005_wi", txs$target, txs$column_name,
                    "alt_dem_cont_scale_clst", "urbanscore_cont_clst", "hab1_cont_scale", "hv104_fctb", # need to add in covariates that don't have confounding ancestors but are needed elsewhere
                    "longnum", "latnum")
 dt.ml <- dt %>% 
