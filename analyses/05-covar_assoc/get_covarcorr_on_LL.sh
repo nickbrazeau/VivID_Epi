0587fd3edc5362c1a239edc86e8f56b1a61c2ece
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-08-02 15:51:25 -0400

    fix string constant on bash submission script

--- analyses/05-covar_assoc/get_covarcorr_on_LL.sh
+++ analyses/05-covar_assoc/get_covarcorr_on_LL.sh
@@ -6,4 +6,4 @@
 #SBATCH --mail-type=all
 #SBATCH --mail-user=nbrazeau@med.unc.edu
 
-Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Epi"); source("analyses/05-covar_assoc/get_covarcorr_on_LL.sh")'
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Epi"); source("analyses/05-covar_assoc/01-covar_assoc_slurm.R")'
