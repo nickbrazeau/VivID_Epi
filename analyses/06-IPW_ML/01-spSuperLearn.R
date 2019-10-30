d82f4834cdda4e865d4fa805ae6a78c3a6777aa4
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-10-30 23:27:51 +0000

    superlearn to base learn

--- analyses/06-IPW_ML/01-spSuperLearn.R
+++ analyses/06-IPW_ML/01-spSuperLearn.R
@@ -8,7 +8,7 @@ library(mlr)
 library(rslurm)
 source("R/00-functions_basic.R")
 source("R/00-IPTW_functions.R")
-source("analyses/06-IPW_ML/00-import_learners_updated.R")
+source("analyses/06-IPW_ML/00-import_learners.R")
 set.seed(44, "L'Ecuyer")
 
 #...............................................................................................
