b362058aab8f384bfa3f440f2db55c964732177d
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-11-02 14:24:06 +0000

    allow man process from EDA for iptw

--- analyses/06-IPW_ML/01-spSuperLearn.R
+++ analyses/06-IPW_ML/01-spSuperLearn.R
@@ -59,10 +59,10 @@ txs$learnerlib <- purrr::map(txs$type, function(x){
 # Manual processing from EDA, 
 # know some of these parameters need to be changed
 #...............................................................................................
-#txs$learnerlib[txs$target == "precip_mean_cont_scale_clst"] <- list(list(mlr::makeLearner("regr.lm", predict.type = "response")))
-#txs$learnerlib[txs$target == "hiv03_fctb"] <- list(list(mlr::makeLearner("classif.logreg", predict.type = "prob")))
-#txs$learnerlib[txs$target == "hv21345_fctb"] <- list(list(mlr::makeLearner("classif.logreg", predict.type = "prob")))
-#txs$learnerlib[txs$target == "ITN_fctb"] <- list(list(mlr::makeLearner("classif.logreg", predict.type = "prob")))
+txs$learnerlib[txs$target == "precip_mean_cont_scale_clst"] <- list(list(mlr::makeLearner("regr.lm", predict.type = "response")))
+txs$learnerlib[txs$target == "hiv03_fctb"] <- list(list(mlr::makeLearner("classif.logreg", predict.type = "prob")))
+txs$learnerlib[txs$target == "hv21345_fctb"] <- list(list(mlr::makeLearner("classif.logreg", predict.type = "prob")))
+txs$learnerlib[txs$target == "ITN_fctb"] <- list(list(mlr::makeLearner("classif.logreg", predict.type = "prob")))
 
 
 
