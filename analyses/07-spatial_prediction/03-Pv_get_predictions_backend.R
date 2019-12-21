820db22c749ca48f1d60ae9c9dc90d1e81da060f
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-12-21 10:41:21 -0500

    sample to one third

--- analyses/07-spatial_prediction/03-Pv_get_predictions_backend.R
+++ analyses/07-spatial_prediction/03-Pv_get_predictions_backend.R
@@ -68,7 +68,7 @@ pred.df <- cbind.data.frame(grid.pred.coords.df,
                 !is.na(cropprop_cont_scale_clst),
                 !is.na(nightlightsmean_cont_scale_clst)) 
 
-pred.df <- pred.df[sample(x=1:nrow(pred.df), size = 1e4), ]
+pred.df <- pred.df[sample(x=1:nrow(pred.df), size = 25e3), ]
 
 #...............................
 # Setup Map Dataframe  
@@ -106,7 +106,7 @@ setwd("analyses/07-spatial_prediction")
 ntry <- nrow(gp.mod.framework)
 sjob <- rslurm::slurm_apply(f = pred_PrevMap_bayes_wrapper, 
                             params = paramsdf, 
-                            jobname = 'Prevmap_predictions',
+                            jobname = 'Prevmap_predictions_large',
                             nodes = ntry, 
                             cpus_per_node = 1, 
                             submit = T,
