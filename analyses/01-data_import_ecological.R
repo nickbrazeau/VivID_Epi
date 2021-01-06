1691f527bbb305ed0fafc0e183565325d7038230
nickbrazeau <nbrazeau@med.unc.edu>
2021-01-06 15:17:57 -0500

    better combo for ecological

--- analyses/01-data_import_ecological.R
+++ analyses/01-data_import_ecological.R
@@ -71,37 +71,47 @@ wtrlns <- sf::read_sf("data/raw_data/hotosm_data/hotosm_cod_waterways_lines_shp/
   dplyr::filter(waterway == "river") %>% 
   dplyr::rename(watertype = waterway) %>% 
   dplyr::select(c("osm_id", "watertype", "geometry"))
+# sanity check
+sf::st_crs(wtrlns)
 
 wtrply <- sf::read_sf("data/raw_data/hotosm_data/hotosm_cod_waterways_polygons_shp/hotosm_cod_waterways_polygons.shp") %>% 
   dplyr::filter(water == "lake") %>% 
   dplyr::rename(watertype = water) %>% 
   dplyr::select(c("osm_id", "watertype", "geometry"))
+# sanity check
+sf::st_crs(wtrply)
 
-wtr <- sf::st_combine(rbind(wtrlns, wtrply))
-wtr <-  sf::st_union( wtr )
 #......................
 # quick look
 #......................
-# sanity check
-sf::st_crs(wtr)
 # plot
 DRCprov <- readRDS("data/map_bases/gadm/gadm36_COD_0_sp.rds")
 ggplot() + 
   geom_sf(data = sf::st_as_sf(DRCprov)) +
-  geom_sf(data = wtr, color = "blue")
+  geom_sf(data = wtrlns, color = "blue") + 
+  geom_sf(data = wtrply, color = "blue") + 
+  geom_sf(data = ge, color = "red")
 
 #......................
 # GC dist
 #......................
-wtrdist <- sf::st_distance(x = ge,
-                           y = wtr,
-                           which = "Great Circle")
+riverdist <- sf::st_distance(x = ge,
+                             y = wtrlns,
+                             which = "Great Circle")
+
+lakedist <- sf::st_distance(x = ge,
+                            y = wtrply,
+                            which = "Great Circle")
 
 
 wtrdist_out <- data.frame(
   hv001 = ge$dhsclust,
-  wtrdist_cont_clst = apply(wtrdist, 1, min)
-)
+  river = apply(riverdist, 1, min),
+  lake = apply(lakedist, 1, min)) %>% 
+  dplyr::group_by(hv001) %>% 
+  dplyr::mutate(wtrdist_cont_clst = min(river, lake)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::select(c("hv001", "wtrdist_cont_clst"))
 
 # check the results
 wtrdist_ge <- wtrdist_out %>% 
@@ -110,7 +120,8 @@ wtrdist_ge <- wtrdist_out %>%
 DRCprov <- readRDS("data/map_bases/gadm/gadm36_COD_0_sp.rds")
 ggplot() + 
   geom_sf(data = sf::st_as_sf(DRCprov)) +
-  geom_sf(data = wtr, color = "blue") +
+  geom_sf(data = wtrlns, color = "blue") + 
+  geom_sf(data = wtrply, color = "blue") + 
   geom_point(data = wtrdist_ge, aes(x = longnum, y = latnum, color = wtrdist_cont_clst)) +
   scale_color_viridis_c("Wtr Dist")
 
@@ -124,3 +135,4 @@ dir.create(path = "data/derived_data/", recursive = T)
 saveRDS(drc_ape, file = "data/derived_data/drc_ape.rds")
 saveRDS(object = wtrdist_out, file = "data/derived_data/hotosm_waterways_dist.rds")
 save(wtrply, wtrlns, file = "data/derived_data/hotosm_waterways.RDA")
+
