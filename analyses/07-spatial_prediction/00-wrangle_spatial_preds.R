5796e18617ac404423dccb5b5e6a9a013af54d76
nickbrazeau <nbrazeau@med.unc.edu>
2021-01-11 13:46:44 -0500

    update spatial pred backend

---
+++ analyses/07-spatial_prediction/00-wrangle_spatial_preds.R
@@ -0,0 +1,173 @@
+#----------------------------------------------------------------------------------------------------
+# Purpose of this script is to get spatial predictions from 
+# spatial models produced from prevmap
+#----------------------------------------------------------------------------------------------------
+source("R/00-functions_maps.R") 
+source("R/00-functions_basic.R")
+library(tidyverse)
+library(drake)
+library(raster)
+library(PrevMap)
+set.seed(48, "L'Ecuyer")
+
+#......................
+# pieces need to manipulate spatial preds
+#......................
+# create bounding box of Central Africa for Speed
+# https://gis.stackexchange.com/questions/206929/r-create-a-boundingbox-convert-to-polygon-class-and-plot/206952
+caf <- as(raster::extent(10, 40,-18, 8), "SpatialPolygons")
+sp::proj4string(caf) <- "+init=epsg:4326"
+
+# create mask 
+DRCprov <- readRDS("data/map_bases/gadm/gadm36_COD_0_sp.rds")
+
+
+#...............................
+# sample coordinates for prediction surface 
+#...............................
+# boundaries for prediction
+poly <- cbind(c(17,32,32,12,12), c(-14,-14,6,6,-14)) 
+grid.pred.coords <- splancs::gridpts(poly, xs=0.1, ys=0.1)
+colnames(grid.pred.coords) <- c("longnum","latnum")
+grid.pred.coords.df <- as.data.frame(grid.pred.coords)
+# convert to sf tidy  
+grid.pred.coords <- sf::st_as_sf(grid.pred.coords.df, coords = c("longnum", "latnum"),
+                                 crs = "+init=epsg:4326")
+
+#...............................
+# extract covariate information for prediction surface 
+#...............................
+# Raster surfaces for risk factors
+riskvars <- c("precip_mean_cont_scale_clst", 
+              "cropprop_cont_scale_clst", "hlthdist_cont_scale_clst")
+
+# take mean aggregate up and then scale like covar
+precipraster <- readRDS("data/derived_data/vividepi_precip_study_period_effsurface.rds") 
+precipraster <- raster::aggregate(precipraster, fact = 2, fun = mean)
+# transform like fitting
+precipraster <- raster::scale(precipraster, center = TRUE, scale = TRUE)
+
+# take mean here since we converted to binary
+cropraster <- readRDS("data/derived_data/vividepi_cropland_surface.rds")
+cropraster <- raster::aggregate(cropraster, fact = 36, fun = mean)
+# transform like fitting
+values(cropraster) <- my.scale(logit(values(cropraster), tol = 1e-3))
+
+
+# here we need to take healthcare walk distance
+# aggregate up and then do log transform so it more closelymatches how we manipulate before
+htlhdistraster <- raster::raster("data/raw_data/hlthdist/2020_walking_only_travel_time_to_healthcare.geotiff")
+sp::identicalCRS(htlhdistraster, caf)
+sp::identicalCRS(htlhdistraster, sf::as_Spatial(DRCprov))
+htlhdistraster <- raster::crop(htlhdistraster, caf)
+htlhdistraster <- raster::mask(htlhdistraster, DRCprov)
+htlhdistraster <- raster::aggregate(htlhdistraster, fact = 12, fun = mean)
+values(htlhdistraster) <- my.scale(log(values(htlhdistraster) + 0.5))
+
+# stack 
+predcovars <- raster::stack(precipraster, cropraster, htlhdistraster)
+names(predcovars) <- riskvars
+
+pred.df <- raster::extract(
+  x = predcovars,
+  y = sf::as_Spatial(grid.pred.coords),
+  buffer = 10000, # max dhs offset
+  fun = mean,
+  na.rm = T,
+  sp = F)
+
+pred.df <- as.data.frame(pred.df)
+pred.df <- cbind.data.frame(grid.pred.coords.df, pred.df)
+
+#......................
+# sanity checks
+#......................
+p1 <- ggplot() +
+  ggspatial::layer_spatial(data = precipraster, aes(fill = stat(band1))) +
+  scale_fill_viridis_c("", na.value = NA) +
+  theme_void()
+p2 <- ggplot() +
+  geom_point(data = pred.df, aes(x = longnum, y = latnum,
+                                 color = precip_mean_cont_scale_clst))  +
+  scale_color_distiller("", type = "div", palette = "RdYlBu", na.value = NA) +
+  theme_void()
+cowplot::plot_grid(p1, p2, nrow = 1)
+
+p1 <- ggplot() +
+  ggspatial::layer_spatial(data = cropraster, aes(fill = stat(band1))) +
+  scale_fill_viridis_c("", na.value = NA) +
+  theme_void()
+p2 <- ggplot() +
+  geom_point(data = pred.df, aes(x = longnum, y = latnum,
+                                 color = cropprop_cont_scale_clst))  +
+  scale_color_distiller("", type = "div", palette = "RdYlBu", na.value = NA) +
+  theme_void()
+cowplot::plot_grid(p1, p2, nrow = 1)
+
+p1 <- ggplot() +
+  ggspatial::layer_spatial(data = htlhdistraster, aes(fill = stat(band1))) +
+  scale_fill_viridis_c("", na.value = NA) +
+  theme_void()
+p2 <- ggplot() +
+  geom_point(data = pred.df, aes(x = longnum, y = latnum,
+                                 color = hlthdist_cont_scale_clst))  +
+  scale_color_distiller("", type = "div", palette = "RdYlBu", na.value = NA) +
+  theme_void()
+cowplot::plot_grid(p1, p2, nrow = 1)
+
+# look at hlth dist which from MAP was very pointed
+p1 <- ggplot() +
+  ggspatial::layer_spatial(data = htlhdistraster_raw, aes(fill = stat(band1))) +
+  scale_fill_viridis_c("", na.value = NA) +
+  theme_void()
+p2 <- ggplot() +
+  ggspatial::layer_spatial(data = htlhdistraster, aes(fill = stat(band1))) +
+  scale_fill_viridis_c("", na.value = NA) +
+  theme_void()
+cowplot::plot_grid(p1, p2, nrow = 1)
+
+
+#...............................
+# Bounds so we only do interpolation
+# (i.e. coerce extrapolation values)
+#...............................
+# precip
+dt <- readRDS("data/derived_data/vividepi_recode_completecases.rds")
+precip <- dt %>% 
+  dplyr::select("hv001", "precip_mean_cont_scale_clst") %>% 
+  dplyr::filter(!duplicated(.))
+max.precip <- max(precip$precip_mean_cont_scale_clst)
+min.precip <- min(precip$precip_mean_cont_scale_clst)
+
+pred.df$precip_mean_cont_scale_clst[pred.df$precip_mean_cont_scale_clst > max.precip] <- max.precip
+pred.df$precip_mean_cont_scale_clst[pred.df$precip_mean_cont_scale_clst < min.precip] <- min.precip
+
+
+# bring in cropland
+crop <- readRDS("data/derived_data/vividepi_cropland_propmeans.rds") 
+max.crop <- max(crop$cropprop_cont_scale_clst)
+min.crop <- min(crop$cropprop_cont_scale_clst)
+pred.df$cropprop_cont_scale_clst[pred.df$cropprop_cont_scale_clst > max.crop] <- max.crop
+pred.df$cropprop_cont_scale_clst[pred.df$cropprop_cont_scale_clst < min.crop] <- min.crop
+
+
+# bring in health care distance/access clst means
+acc <- readRDS("data/derived_data/vividepi_hlthdist_clstmeans.rds") 
+max.acc <- max(acc$hlthdist_cont_scale_clst)
+min.acc <- min(acc$hlthdist_cont_scale_clst)
+pred.df$hlthdist_cont_scale_clst[pred.df$hlthdist_cont_scale_clst > max.acc] <- max.acc
+pred.df$hlthdist_cont_scale_clst[pred.df$hlthdist_cont_scale_clst < min.acc] <- min.acc
+
+
+#..............................................................
+# Convert to raster for sampling
+#..............................................................
+covar.rstr.pred <- raster::rasterFromXYZ(pred.df, 
+                                         res = c(0.1, 0.1),
+                                         crs = "+init=epsg:4326")
+
+#......................
+# save out
+#......................
+# save this out for spatial preds covar
+saveRDS(covar.rstr.pred, file = "data/derived_data/vividepi_spatial_covar_feature_engineer.rds")
--- analyses/07-spatial_prediction/03-Pv_get_predictions_backend.R
+++ analyses/07-spatial_prediction/03-Pv_get_predictions_backend.R
@@ -10,18 +10,10 @@ library(raster)
 library(PrevMap)
 set.seed(48, "L'Ecuyer")
 
-#......................
-# pieces need to manipulate spatial preds
-#......................
-# create bounding box of Central Africa for Speed
-# https://gis.stackexchange.com/questions/206929/r-create-a-boundingbox-convert-to-polygon-class-and-plot/206952
-caf <- as(raster::extent(10, 40,-18, 8), "SpatialPolygons")
-sp::proj4string(caf) <- "+init=epsg:4326"
 
-# create mask 
-DRCprov <- readRDS("data/map_bases/gadm/gadm36_COD_0_sp.rds")
-sf::st_crs(DRCprov)
-
+#............................................................
+# set up spatial model
+#...........................................................
 #......................
 # Import Data
 #......................
@@ -32,152 +24,11 @@ gp.mod.framework <- tibble::tibble(name = c("intercept", "covars"),
                                    mcmc = list(intercept, covar))
 
 
-#...............................
-# sample coordinates for prediction surface 
-#...............................
-# boundaries for prediction
-poly <- cbind(c(17,32,32,12,12), c(-14,-14,6,6,-14)) 
-grid.pred.coords <- splancs::gridpts(poly, xs=0.1, ys=0.1)
-colnames(grid.pred.coords) <- c("longnum","latnum")
-grid.pred.coords.df <- as.data.frame(grid.pred.coords)
-# convert to sf tidy  
-grid.pred.coords <- sf::st_as_sf(grid.pred.coords.df, coords = c("longnum", "latnum"),
-                                 crs = "+init=epsg:4326")
-
-#...............................
-# extract covariate information for prediction surface 
-#...............................
-# Raster surfaces for risk factors
-riskvars <- c("precip_mean_cont_scale_clst", 
-              "cropprop_cont_scale_clst", "hlthdist_cont_scale_clst")
-
-# take mean aggregate up and then scale like covar
-precipraster <- readRDS("data/derived_data/vividepi_precip_study_period_effsurface.rds") 
-precipraster <- raster::aggregate(precipraster, fact = 2, fun = mean)
-# transform like fitting
-precipraster <- raster::scale(precipraster, center = TRUE, scale = TRUE)
-
-# take mean here since we converted to binary
-cropraster <- readRDS("data/derived_data/vividepi_cropland_surface.rds")
-cropraster <- raster::aggregate(cropraster, fact = 36, fun = mean)
-# transform like fitting
-values(cropraster) <- my.scale(logit(values(cropraster), tol = 1e-3))
-
-
-# here we need to take healthcare walk distance
-# aggregate up and then do log transform so it more closelymatches how we manipulate before
-htlhdistraster <- raster::raster("data/raw_data/hlthdist/2020_walking_only_travel_time_to_healthcare.geotiff")
-sp::identicalCRS(htlhdistraster, caf)
-sp::identicalCRS(htlhdistraster, sf::as_Spatial(DRCprov))
-htlhdistraster <- raster::crop(htlhdistraster, caf)
-htlhdistraster <- raster::mask(htlhdistraster, DRCprov)
-htlhdistraster <- raster::aggregate(htlhdistraster, fact = 12, fun = mean)
-values(htlhdistraster) <- my.scale(log(values(htlhdistraster) + 0.5))
-
-# stack 
-predcovars <- raster::stack(precipraster, cropraster, htlhdistraster)
-names(predcovars) <- riskvars
-
-pred.df <- raster::extract(
-  x = predcovars,
-  y = sf::as_Spatial(grid.pred.coords),
-  buffer = 10000, # max dhs offset
-  fun = mean,
-  na.rm = T,
-  sp = F)
-
-pred.df <- as.data.frame(pred.df)
-pred.df <- cbind.data.frame(grid.pred.coords.df, pred.df)
-
 #......................
-# sanity checks
+# read in prediction df and downsample for spatial predictions
 #......................
-p1 <- ggplot() +
-  ggspatial::layer_spatial(data = precipraster, aes(fill = stat(band1))) +
-  scale_fill_viridis_c("", na.value = NA) +
-  theme_void()
-p2 <- ggplot() +
-    geom_point(data = pred.df, aes(x = longnum, y = latnum,
-                                   color = precip_mean_cont_scale_clst))  +
-  scale_color_distiller("", type = "div", palette = "RdYlBu", na.value = NA) +
-  theme_void()
-cowplot::plot_grid(p1, p2, nrow = 1)
-
-p1 <- ggplot() +
-  ggspatial::layer_spatial(data = cropraster, aes(fill = stat(band1))) +
-  scale_fill_viridis_c("", na.value = NA) +
-  theme_void()
-p2 <- ggplot() +
-  geom_point(data = pred.df, aes(x = longnum, y = latnum,
-                                 color = cropprop_cont_scale_clst))  +
-  scale_color_distiller("", type = "div", palette = "RdYlBu", na.value = NA) +
-  theme_void()
-cowplot::plot_grid(p1, p2, nrow = 1)
-
-p1 <- ggplot() +
-  ggspatial::layer_spatial(data = htlhdistraster, aes(fill = stat(band1))) +
-  scale_fill_viridis_c("", na.value = NA) +
-  theme_void()
-p2 <- ggplot() +
-  geom_point(data = pred.df, aes(x = longnum, y = latnum,
-                                 color = hlthdist_cont_scale_clst))  +
-  scale_color_distiller("", type = "div", palette = "RdYlBu", na.value = NA) +
-  theme_void()
-cowplot::plot_grid(p1, p2, nrow = 1)
-
-# look at hlth dist which from MAP was very pointed
-p1 <- ggplot() +
-  ggspatial::layer_spatial(data = htlhdistraster_raw, aes(fill = stat(band1))) +
-  scale_fill_viridis_c("", na.value = NA) +
-  theme_void()
-p2 <- ggplot() +
-  ggspatial::layer_spatial(data = htlhdistraster, aes(fill = stat(band1))) +
-  scale_fill_viridis_c("", na.value = NA) +
-  theme_void()
-cowplot::plot_grid(p1, p2, nrow = 1)
-
-
-#...............................
-# Bounds so we only do interpolation
-# (i.e. coerce extrapolation values)
-#...............................
-# precip
-dt <- readRDS("data/derived_data/vividepi_recode_completecases.rds")
-precip <- dt %>% 
-  dplyr::select("hv001", "precip_mean_cont_scale_clst") %>% 
-  dplyr::filter(!duplicated(.))
-max.precip <- max(precip$precip_mean_cont_scale_clst)
-min.precip <- min(precip$precip_mean_cont_scale_clst)
-
-pred.df$precip_mean_cont_scale_clst[pred.df$precip_mean_cont_scale_clst > max.precip] <- max.precip
-pred.df$precip_mean_cont_scale_clst[pred.df$precip_mean_cont_scale_clst < min.precip] <- min.precip
-
-
-# bring in cropland
-crop <- readRDS("data/derived_data/vividepi_cropland_propmeans.rds") 
-max.crop <- max(crop$cropprop_cont_scale_clst)
-min.crop <- min(crop$cropprop_cont_scale_clst)
-pred.df$cropprop_cont_scale_clst[pred.df$cropprop_cont_scale_clst > max.crop] <- max.crop
-pred.df$cropprop_cont_scale_clst[pred.df$cropprop_cont_scale_clst < min.crop] <- min.crop
-
-
-# bring in health care distance/access clst means
-acc <- readRDS("data/derived_data/vividepi_hlthdist_clstmeans.rds") 
-max.acc <- max(acc$hlthdist_cont_scale_clst)
-min.acc <- min(acc$hlthdist_cont_scale_clst)
-pred.df$hlthdist_cont_scale_clst[pred.df$hlthdist_cont_scale_clst > max.acc] <- max.acc
-pred.df$hlthdist_cont_scale_clst[pred.df$hlthdist_cont_scale_clst < min.acc] <- min.acc
-
-
-#..............................................................
-# Convert to raster for sampling
-#..............................................................
-covar.rstr.pred <- raster::rasterFromXYZ(pred.df, 
-                                         res = c(0.1, 0.1),
-                                         crs = "+init=epsg:4326")
-
-# save this out for later
-saveRDS(covar.rstr.pred, file = "data/derived_data/vividepi_spatial_covar_feature_engineer.rds")
+# read in spatial raster preds
+covar.rstr.pred <- readRDS("data/derived_data/vividepi_spatial_covar_feature_engineer.rds")
 
 # down sample
 covar.rstr.pred.downsmpl <- raster::sampleRandom(covar.rstr.pred, 
@@ -189,19 +40,21 @@ covar.rstr.pred.downsmpl <- raster::sampleRandom(covar.rstr.pred,
 colnames(covar.rstr.pred.downsmpl)[1:2] <- c("longnum", "latnum")
 
 #...............................
-# Setup Map Dataframe  
+# bring together Map Dataframe  
 #...............................
-# set up grid.pred
+# bring in grid.pred
 gp.mod.framework$grid.pred <- list(covar.rstr.pred.downsmpl[,c("longnum", "latnum")],
                                    covar.rstr.pred.downsmpl[,c("longnum", "latnum")])
 
-# set up predictors
+# bring in predictors
 gp.mod.framework$predictors <- list(NULL, covar.rstr.pred.downsmpl[,c("precip_mean_cont_scale_clst", 
                                                                       "cropprop_cont_scale_clst",
                                                                       "hlthdist_cont_scale_clst")])
 
 
-
+#............................................................
+# Running Spatial Model
+#...........................................................
 # make wrapper
 pred_PrevMap_bayes_wrapper <- function(mcmc, grid.pred, predictors){
   ret <- PrevMap::spatial.pred.binomial.Bayes(object = mcmc, 
