2ce05a7d1ec0a10302baee031915ec748ebc4f8b
nickbrazeau <nbrazeau@med.unc.edu>
2021-01-15 10:58:07 -0500

    fix backend weight check

--- analyses/06-IPW_ML/02-check_weights_backend_rslurm.R
+++ analyses/06-IPW_ML/02-check_weights_backend_rslurm.R
@@ -2,17 +2,37 @@
 # Purpose of this script is to check for 
 # covariate balance after applying the weights
 #----------------------------------------------------------------------------------------------------
-library(drake)
+library(rslurm)
 library(tidyverse)
 library(energy)
 source("R/00-IPTW_functions.R")
 set.seed(48, "L'Ecuyer")
 
 #............................................................
-# Import Data and superlearner results
+# Import Data
 #............................................................
 dt <- readRDS("data/derived_data/vividepi_recode_completecases.rds")
-params <- readRDS("results/ensembl_cvRisk_paramdf.RDS")
+
+params <- readRDS("analyses/06-IPW_ML/_rslurm_vivid_spSL/params.RDS")
+SLpaths <- list.files("analyses/06-IPW_ML/_rslurm_vivid_spSL/", 
+                      pattern = ".RDS", full.names = T)
+
+SLpaths <- SLpaths[!c(grepl("params.RDS", SLpaths) | grepl("f.RDS", SLpaths))]
+
+# sort properly to match rows in df
+SLpaths <- tibble::tibble(SLpaths = SLpaths) %>% 
+  mutate(order = stringr::str_extract(basename(SLpaths), "[0-9]+"),
+         order = as.numeric(order)) %>% 
+  dplyr::arrange(order) %>% 
+  dplyr::select(-c(order)) %>% 
+  unlist(.)
+params$ensembl_cvRisk <- purrr::map(  SLpaths, function(x){ ret <- readRDS(x); return(ret[[1]]) }  )
+
+#............................................................
+# Get IPTW Estimates
+#............................................................
+params$SLpreds <-purrr::map(params$ensembl_cvRisk, "SL.predictions")
+params$iptw <- purrr::pmap(params[,c("task", "SLpreds")], get_iptw_prob)
 
 #............................................................
 # Make Model Map
@@ -34,7 +54,9 @@ select_samples_by_iptw_and_feattarget <- function(target, feat, iptw, data){
 # in the energy function 
 #............................................................
 modelmap.ml <- params %>% 
-  dplyr::select(c("task", "iptw", "target"))
+  dplyr::mutate(target = purrr::map(.$task, mlr::getTaskTargetNames)) %>% 
+  dplyr::select(c("task", "iptw", "target")) %>% 
+  tidyr::unnest(col = target)
 
 modelmap.ml <- replicate(n = 100, modelmap.ml,
                          simplify = F) %>% 
@@ -61,7 +83,9 @@ modelmap.ml <- modelmap.ml %>%
 
 # get base target-feats too
 modelmap.nowi <- params %>% 
-  dplyr::select(c("task", "target")) 
+  dplyr::mutate(target = purrr::map(.$task, mlr::getTaskTargetNames)) %>% 
+  dplyr::select(c("task", "target")) %>% 
+  tidyr::unnest(col = target)
 
 modelmap.nowi$feat <- map(modelmap.nowi$task, mlr::getTaskFeatureNames)
 modelmap.nowi$data <- lapply(1:nrow(modelmap.nowi), function(x) return(dt))
@@ -82,7 +106,7 @@ modelmap <- rbind.data.frame(modelmap.nowi, modelmap.ml)
 # Energy function for slurm
 #............................................................
 
-slurm_calc_corr <- function(covar1, covar2, data, lvl, id){
+slurm_calc_corr <- function(covar1, covar2, data, lvl){
   x1 <- unlist( data[,covar1] )
   x2 <- unlist( data[,covar2] )
   if(is.factor(x1)){
@@ -98,81 +122,23 @@ slurm_calc_corr <- function(covar1, covar2, data, lvl, id){
 }
 
 
-#............................................................
-# parallelize with drake
-#...........................................................
-workers <- 512 # nodes to ask for, fewer nodes, less expensive for reading in data and not placing burden on scheduler
-library(drake)
+#................................
+# send it out with rSLURM
+#................................
 
-#......................
-# functions for drake
-#......................
-drake_wrapper <- function(batchset_df) {
-  
-  # call future
-  no_cores <- future::availableCores() - 1
-  if (no_cores > 1) {
-    future::plan(future::multicore, workers = no_cores)
-  } else {
-    future::plan("sequential")
-  }
-  
-  
-  #......................
-  # run batches to not overload scheduler
-  #......................
-  batchset_df$energcorr <- furrr::future_pmap(batchset_df, slurm_calc_corr)
-  
-  # now write out
-  dir.create("/pine/scr/n/f/nfb/Projects/VivID_Epi/06-IPW_ML/wi_covar_corr/", recursive = T)
-  saveRDS(batchset_df,
-          file = paste0("/pine/scr/n/f/nfb/Projects/VivID_Epi/06-IPW_ML/wi_covar_corr/",
-                        "batchset_", unique(batchset_df$id), ".RDS"))
-  
-}
-
-#......................
-# make batches
-#......................
-batchnum <- sort( rep(1:workers, ceiling(nrow(modelmap) / workers)) )
-batchnum <- batchnum[1 :nrow(modelmap)]
-
-modelmap_nested <- modelmap %>%
-  dplyr::mutate(id = batchnum,
-                batchset = batchnum) %>%
-  dplyr::group_by(batchset) %>%
-  tidyr::nest() %>%
-  dplyr::ungroup()
-
-
-#......................
-# make drake plan
-#......................
-batch_names <- paste0("batch", modelmap_nested$batchset)
-plan <- drake::drake_plan(
-  runs = target(
-    drake_wrapper(data),
-    transform = map(
-      .data = !!modelmap_nested,
-      .names = !!batch_names
-    )
-  )
-)
-
-#......................
-# call drake to send out to slurm
-#......................
-options(clustermq.scheduler = "slurm",
-        clustermq.template = "drake_clst/slurm_clustermq_LL_short.tmpl")
-make(plan,
-     parallelism = "clustermq",
-     jobs = nrow(modelmap_nested),
-     log_make = "wi_covar_ipw_drake.log", verbose = 4,
-     log_progress = TRUE,
-     log_build_times = FALSE,
-     recoverable = FALSE,
-     history = FALSE,
-     session_info = FALSE,
-     garbage_collection = TRUE,
-     lock_envir = FALSE,
-     lock_cache = FALSE)
+# for slurm on LL
+setwd("analyses/06-IPW_ML/")
+sjob <- rslurm::slurm_apply(f = slurm_calc_corr, 
+                            params = modelmap, 
+                            jobname = 'covar_IPTW_balance',
+                            nodes = 512, 
+                            cpus_per_node = 1, 
+                            submit = T,
+                            slurm_options = list(mem = 32000,
+                                                 array = sprintf("0-%d%%%d", 
+                                                                 512, 
+                                                                 128),
+                                                 'cpus-per-task' = 1,
+                                                 error =  "%A_%a.err",
+                                                 output = "%A_%a.out",
+                                                 time = "5:00:00"))
